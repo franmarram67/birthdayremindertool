1a82bb11ad4a8b4b09abe0747df5fbb7338157e5
franmarram67 <pakinro7rai@gmail.com>
2026-06-25 16:53:28 +0200

    💡 feat: create contact model, migration and livewire component, link this component to a route. publish navbar and navlist components for fluxui. alter .env.example to match more of what we need

---
+++ resources/views/pages/⚡contacts.blade.php
@@ -0,0 +1,13 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div class="border dark:border-zinc-400 w-full h-full rounded-lg dark:bg-zinc-700">
+    <h1>Contacts</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+
+    Route::livewire('contacts', 'pages::contacts')->name('contacts');
 });
 
 require __DIR__.'/settings.php';
