f8674b222395e95620d920cd8653b238857b265f
franmarram67 <pakinro7rai@gmail.com>
2026-06-26 17:38:41 +0200

    💡 feat: remove navbar/navlist published fluxui component, improve contacts page and start shaping its form, add row component for contact rows, experimental

---
+++ resources/views/components/contacts/row.blade.php
@@ -0,0 +1,12 @@
+@props([
+    'src' => "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg"
+])
+
+<div class="py-2 border-b border-zinc-400 mx-4 flex justify-between items-center">
+    <img class="rounded-lg size-12" src="{{ $src }}" />
+    <h2>Default Name</h2>
+    <div class="flex justify-between items-center">
+        <flux:badge class="mr-4" color="green">01-01-2000</flux:badge>
+        <flux:button variant="primary" class="cursor-pointer" ><flux:icon.pencil class="size-4" />Edit Contact</flux:button>
+    </div>
+</div>
--- resources/views/pages/⚡contacts.blade.php
+++ resources/views/pages/⚡contacts.blade.php
@@ -4,10 +4,30 @@ use Livewire\Component;
 
 new class extends Component
 {
-    //
+    public $defaultContactImageSrc = "https://static.vecteezy.com/system/resources/thumbnails/009/734/564/small/default-avatar-profile-icon-of-social-media-user-vector.jpg"; 
 };
 ?>
 
 <div class="border dark:border-zinc-400 w-full h-full rounded-lg dark:bg-zinc-700">
-    <h1>Contacts</h1>
+    <flux:modal name="create-contact">
+        <flux:heading size="lg">{{ __('Create a Contact') }}</flux:heading>
+    </flux:modal>
+    <div class="flex justify-between items-center">
+        <div class="m-4">
+            <flux:heading size="xl">{{ __('Contacts') }}</flux:heading>
+            <flux:subheading>{{ __('Add or manage contacts') }}</flux:subheading>
+        </div>
+        <flux:modal.trigger name="create-contact">
+            <flux:button class="mr-4 cursor-pointer" variant="primary">
+                <flux:icon.plus/>{{ __('Create Contact') }}
+            </flux:button>
+        </flux:modal.trigger>
+    </div>
+    <div class="border-b border-zinc-400 mx-4"></div>
+    <div>
+        <x-contacts.row :src="$defaultContactImageSrc"/>
+        <x-contacts.row/>
+        <x-contacts.row/>
+        <x-contacts.row/>
+    </div>
 </div>
