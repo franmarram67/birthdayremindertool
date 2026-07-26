1be18c16b1f6aafd9206e21fd194444bbe977cf8
franmarram67 <pakinro7rai@gmail.com>
2026-07-26 23:12:15 +0200

    🚧 wip: make progress on search select, build proper styling, make it so elements are hidden if they don't match the search, start tidying up the selection of the element

--- resources/views/pages/⚡calendar.blade.php
+++ resources/views/pages/⚡calendar.blade.php
@@ -29,15 +29,95 @@ new class extends Component
         <div class="m-4 flex items-center">
             <flux:field>
                 <flux:label>Month</flux:label>
-                {{-- <flux:select placeholder="Choose a month..."></flux:select> --}}
-                {{-- <select class="appearance-none [:where(&)]:w-full ps-3 pe-10 block h-10 py-2 text-base sm:text-sm leading-[1.375rem] rounded-lg shadow-xs border bg-white dark:bg-white/10 dark:disabled:bg-white/[7%] text-zinc-700 dark:text-zinc-300 disabled:text-zinc-500 dark:disabled:text-zinc-400 has-[option.placeholder:checked]:text-zinc-400 dark:has-[option.placeholder:checked]:text-zinc-400 dark:[&>option]:bg-zinc-700 dark:[&>option]:text-white disabled:shadow-none border border-zinc-200 border-b-zinc-300/80 dark:border-white/10">
-                    <option>WTF</option>
-                </select> --}}
-                <div class="border dark:border-zinc-400 rounded-lg dark:bg-zinc-600 flex justify-center items-center [&:hover>svg]:stroke-zinc-300 dark:hover:border-zinc-300 relative">
-                    <input type="text" placeholder="Choose a month..." class="p-[0.56rem] focus:outline-0 text-zinc-300 text-sm w-40" />
-                    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg' class="flex justify-center items-center absolute z-10 right-1.5 stroke-zinc-400 "><path d='M8 9L12 5L16 9' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/><path d='M16 15L12 19L8 15' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/></svg>
-                    <div class="absolute border dark:border-zinc-400 rounded-lg dark:bg-zinc-600 w-40 h-9.5  p-[0.56rem] mt-0.5 top-9.5 left-0">
-                        {{-- <div>January</div> --}}
+                <div 
+                    class="flex justify-center items-center [&:hover>svg]:stroke-zinc-300  relative" 
+                    x-data="{ 
+                        focusSearchSelect: false,
+                        searchSelect(value) {
+                            let allSearchElements = document.querySelectorAll('.search-element');
+                            let numberOfSearchElements = allSearchElements.length;
+                            let numberOfHiddenSearchElements = 0;
+                            allSearchElements.forEach((element) => {
+                                let elementText = element.innerText.toLowerCase();
+                                if (!elementText.includes(value.toLowerCase())) {
+                                    element.classList.add('hidden');
+                                    numberOfHiddenSearchElements++;
+                                } else {
+                                    element.classList.remove('hidden');
+                                }
+                            });
+                            let defaultSearch = document.getElementById('default-search');
+                            if (numberOfHiddenSearchElements >= numberOfSearchElements) {
+                                defaultSearch.classList.remove('hidden');
+                            } else {
+                                defaultSearch.classList.add('hidden');
+                            }
+                        },
+                        selectElement(element) {
+                            let number = element.getAttribute('data-number');
+                            let text = element.innerText;
+                            console.log(number);
+                            console.log(text);
+                            this.focusSearchSelect = false;
+                            console.log(this.focusSearchSelect);
+                        }
+                    }" 
+                    @click.outside="focusSearchSelect = false" 
+                    @click="focusSearchSelect = true"
+                >
+                    <input 
+                        type="text" 
+                        placeholder="Choose a month..." 
+                        class="p-[0.56rem] focus:outline-0 text-zinc-300 text-sm w-40 border dark:border-zinc-400 rounded-lg dark:bg-zinc-600 dark:hover:border-zinc-300"
+                        :class="focusSearchSelect ? 'border-b-0 rounded-b-none dark:border-zinc-300!' : ''"
+                        @input="searchSelect($el.value)" 
+                    />
+                    <svg 
+                        width='24' 
+                        height='24' 
+                        viewBox='0 0 24 24' 
+                        fill='none' 
+                        xmlns='http://www.w3.org/2000/svg' 
+                        class="flex justify-center items-center absolute z-10 right-1.5 dark:stroke-zinc-400"
+                        :class="focusSearchSelect ? 'dark:stroke-zinc-300!' : ''" 
+                    >
+                        <path d='M8 9L12 5L16 9' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
+                        <path d='M16 15L12 19L8 15' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round'/>
+                    </svg>
+                    <span 
+                        class="absolute border-t border-t-zinc-500 w-39.5 h-0 top-9.5 left-px z-10"
+                        :class="focusSearchSelect ? '' : 'hidden'"
+                    ></span>
+                    <div 
+                        class="absolute border dark:border-zinc-300 rounded-lg dark:bg-zinc-600 w-40 max-h-38 mt-0 top-9.5 left-0 border-t-0 rounded-t-none overflow-scroll" 
+                        :class="focusSearchSelect ? '' : 'hidden'"
+                        x-data="{ 
+                            monthsOfYear: {
+                                1: 'January',
+                                2: 'February',
+                                3: 'March',
+                                4: 'April',
+                                5: 'May',
+                                6: 'June',
+                                7: 'July',
+                                8: 'August',
+                                9: 'September',
+                                10: 'October',
+                                11: 'November',
+                                12: 'December',
+                            }
+                        }"
+                    >
+                        <div class="text-zinc-400 text-sm p-[0.56rem] hidden" id="default-search">No results found.</div>
+                        <template x-for="(value, index) in monthsOfYear">
+                            <div 
+                                class="text-zinc-300 text-sm p-[0.56rem] hover:bg-zinc-500 search-element" 
+                                :data-number="index" 
+                                x-text="value"
+                                @click="selectElement($el)"
+                            >
+                            </div>
+                        </template>
                     </div>
                 </div>
             </flux:field>
