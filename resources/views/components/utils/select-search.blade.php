e8bd0e061cd7b97e73b0c9ea688ac855042ce5ed
franmarram67 <pakinro7rai@gmail.com>
2026-08-03 22:38:14 +0200

    🚧 wip: continue working on hooking up inputs with livewire, set form submit to livewire function

--- resources/views/components/utils/select-search.blade.php
+++ resources/views/components/utils/select-search.blade.php
@@ -44,7 +44,7 @@
         }
     }" 
     @click.outside="focusSearchSelect = false" @click="focusSearchSelect = true">
-    <input type="hidden" value="" name="search-number" wire:model="{{ $inputHiddenModel }}"/>
+    <input type="hidden" value="" name="search-number" wire:model="{{ $inputHiddenModel }}" x-effect="$wire.{{ $inputHiddenModel }} = $el.value"/>
     <input type="text" placeholder="Choose a month..."
         class="p-[0.56rem] focus:outline-0 text-zinc-300 text-sm w-40 border dark:border-zinc-400 rounded-lg dark:bg-zinc-600 dark:hover:border-zinc-300"
         :class="focusSearchSelect ? 'border-b border-b-transparent rounded-b-none dark:border-zinc-300!' : ''"
--- resources/views/pages/⚡calendar.blade.php
+++ resources/views/pages/⚡calendar.blade.php
@@ -17,6 +17,11 @@ new class extends Component
         }
         return $months;
     }
+
+    public function searchCalendar()
+    {
+        dd($this->month);
+    }
 };
 ?>
 <div class="border dark:border-zinc-400 w-full h-full rounded-lg dark:bg-zinc-700">
@@ -28,25 +33,27 @@ new class extends Component
     </div>
     <div class="border-b border-zinc-400 mx-4"></div>
     <div class="flex justify-between items-center">
-        <div class="m-4 flex items-center">
-            <flux:field>
-                <flux:label>Month</flux:label>
-                <x-utils.select-search :inputHiddenModel="'month'">
-                @foreach ($this->getMonthsOfYearForSelectSearch() as $monthNumber => $monthName)
-                    <x-utils.select-search-element :key="$monthNumber" :value="$monthName" />
-                @endforeach
-                </x-utils.select-search>
-            </flux:field>
-            <flux:field class="ml-4">
-                <flux:label>Year</flux:label>
-                <flux:select placeholder="Choose a year...">
-                    <flux:select.option>1000</flux:select.option>
-                </flux:select>
-            </flux:field>
-            <flux:field class="ml-4">
-                <span class="h-[17.5px]"></span>
-                <flux:button type="submit" variant="primary">{{ __('Search') }}</flux:button>
-            </flux:field>
-        </div>
+        <form wire:submit="searchCalendar">
+            <div class="m-4 flex items-center">
+                <flux:field>
+                    <flux:label>Month</flux:label>
+                    <x-utils.select-search :inputHiddenModel="'month'">
+                    @foreach ($this->getMonthsOfYearForSelectSearch() as $monthNumber => $monthName)
+                        <x-utils.select-search-element :key="$monthNumber" :value="$monthName" />
+                    @endforeach
+                    </x-utils.select-search>
+                </flux:field>
+                <flux:field class="ml-4">
+                    <flux:label>Year</flux:label>
+                    <flux:select placeholder="Choose a year...">
+                        <flux:select.option>1000</flux:select.option>
+                    </flux:select>
+                </flux:field>
+                <flux:field class="ml-4">
+                    <span class="h-[17.5px]"></span>
+                    <flux:button type="submit" variant="primary">{{ __('Search') }}</flux:button>
+                </flux:field>
+            </div>
+        </form>
     </div>
 </div>
